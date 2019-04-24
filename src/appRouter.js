0a4a79fd69b1eb40a6580a6f96dc5cb6a28c6b8f
adeptmarlon <marlon.d@office.adept.co.za>
2019-04-24 16:55:27 +0200

    added new theme

--- src/appRouter.js
+++ src/appRouter.js
@@ -1,17 +1,26 @@
 import VueRouter from 'vue-router'
 import App from '@/App';
 import Home from '@/components/Home';
+import AddBlog from '@/components/AddBlog';
 
 export default new VueRouter({
     routes: [{
             path: '/',
             name: 'app',
-            component: App
-        },
-        {
-            path: '/home',
-            name: 'home',
-            component: Home
+            component: App,
+            redirect: '/home',
+            children: [{
+                    path: '/home',
+                    name: 'home',
+                    component: Home
+                },
+                {
+                    path: '/addBlog',
+                    name: 'addBlog',
+                    component: AddBlog
+                }
+            ]
         }
+
     ]
 })
